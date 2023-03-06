c1d942639849fc20c38e5a467d89ed7cf50c1bed
hoangtran <tranhuuhoang190594@gmail.com>
2023-03-06 10:02:39 +0700

    fix api mẫu login

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -6,14 +6,24 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 #[Route('/api')]
 class ApiController extends AbstractController
 {
-    #[Route('/login', name: 'app_api_login')]
-    public function index(Request $request): Response
+    #[Route('/', name: 'app_api')]
+    public function index(): Response
     {
-        $userName = $request->request->get('username');
-        $password = $request->request->get('password');
+        return new JsonResponse(['controller_name' => 'ApiController']);
+        // return $this->render('api/index.html.twig', [
+        //     'controller_name' => 'ApiController',
+        // ]);
+    }
+
+    #[Route('/login', name: 'app_api_login', methods: ['POST'])]
+    public function login(Request $request ): Response
+    {
+        $userName = $request->query->get('username');
+        $password = $request->query->get('password');
 
         $dataRes = [
             "token"=>"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwicm9sZSI6ImFkbWluIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c",
