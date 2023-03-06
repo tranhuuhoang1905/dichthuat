d0792048e077e0cc5c7191056e9349343fb18be8
hoangtran <tranhuuhoang190594@gmail.com>
2023-03-06 10:41:55 +0700

    fix lỗi plagform

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -6,7 +6,6 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\Request;
 #[Route('/api')]
 class ApiController extends AbstractController
 {
@@ -17,23 +16,5 @@ class ApiController extends AbstractController
         // return $this->render('api/index.html.twig', [
         //     'controller_name' => 'ApiController',
         // ]);
-    }
-
-    #[Route('/login', name: 'app_api_login', methods: ['POST'])]
-    public function login(Request $request ): Response
-    {
-        // dump($request);die;
-        $userName = $request->request->get('username');
-        $password = $request->request->get('password');
-
-        $dataRes = [
-            "token"=>"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwicm9sZSI6ImFkbWluIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c",
-            "user"=>[
-                "id"=>"1234567890",
-                "name"=>$userName,
-                "role"=>"admin"
-            ]
-        ];
-        return new JsonResponse($dataRes);
     }
 }
