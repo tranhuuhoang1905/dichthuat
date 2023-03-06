771fc5f95962a886a11807aef56e35dac8104890
hoangtran <tranhuuhoang190594@gmail.com>
2023-03-06 09:32:31 +0700

    test api login

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -9,12 +9,20 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 #[Route('/api')]
 class ApiController extends AbstractController
 {
-    #[Route('/', name: 'app_api')]
-    public function index(): Response
+    #[Route('/login', name: 'app_api_login')]
+    public function index(Request $request): Response
     {
-        return new JsonResponse(['controller_name' => 'ApiController']);
-        // return $this->render('api/index.html.twig', [
-        //     'controller_name' => 'ApiController',
-        // ]);
+        $userName = $request->request->get('username');
+        $password = $request->request->get('password');
+
+        $dataRes = [
+            "token"=>"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwicm9sZSI6ImFkbWluIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c",
+            "user"=>[
+                "id"=>"1234567890",
+                "name"=>$userName,
+                "role"=>"admin"
+            ]
+        ];
+        return new JsonResponse($dataRes);
     }
 }
